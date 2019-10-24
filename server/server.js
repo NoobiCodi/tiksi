45be7bfd04907aeb2422712c6318384d29c794ed
NoobiCodi <35873522+NoobiCodi@users.noreply.github.com>
2019-10-24 22:49:25 +0200

    Improvement (README and server)

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
 // your backend...
 
-console.log('Backend on !');
+// remove the '//' at the beginning and you will see that the script will run when you will start your server
+// console.log('Backend on !');
